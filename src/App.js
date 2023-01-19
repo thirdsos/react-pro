729b93f5fdb02c54c36a50a6efd77dfcc69ba946
Pongsan Sukroey <>
2023-01-19 15:39:16 +0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 // import './App.css';
 import Navbar from './components/Navbar';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import {Switch} from 'react-router-dom'
 import Home from './components/Home';
-import Login from './components/Login';
+
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
@@ -16,15 +14,7 @@ function App() {
   const [isLogin, setIsLogin] = useState(false)
   console.log(isLogin)
   return (
-    <Router>
-      <Navbar />
-      <Container style={{ paddingTop: 10 }}>
-        <Routes>
-          <Route exact path="/login" element={<Login callback={setIsLogin} isLogin={isLogin} />} />
-          <Route exact path="/" element={<Home isLogin={isLogin} />} />
-        </Routes>
-      </Container>
-    </Router>
+    <Home isLogin={isLogin} />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="login" element={<Login />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
